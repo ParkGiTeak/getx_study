9ddf388596b4c57807eb27e28ab9c4815a1093cf
ParkGiTeak <rl980901@naver.com>
2026-01-13 12:12:55 +0900

    feat: github User Search 브랜치에서는 getx_basic 패키지 제거

--- lib/getx_basic/step1/detail_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class DetailController extends GetxController {
-  var name = ''.obs;
-
-  void updateName(String val) {
-    name.value = val;
-  }
-}
--- lib/getx_basic/step2/input_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class InputController extends GetxController {
-  var name = ''.obs;
-
-  void updateName(String val) {
-    name.value = val;
-  }
-}
